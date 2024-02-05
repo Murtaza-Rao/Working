fc5631aeec8dc2301326068c4370ee9718b74b78
Murtaza-Rao <155832200+Murtaza-Rao@users.noreply.github.com>
2024-02-05 06:58:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function onYesClick() {
+    document.getElementById("body").classList.add("yes-clicked");
+}
+
+function onNoClick() {
+  document.getElementById("btnDiv").classList.add("clicked");
+}
